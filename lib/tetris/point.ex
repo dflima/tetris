d54c2bbb27dd65f8fa1e0d3e659e434bb40b1ef7
Danilo <danilo.lima@sumup.com>
2020-11-13 18:25:53 -0300

    :tada: first commit

---
+++ lib/tetris/point.ex
@@ -0,0 +1,19 @@
+defmodule Tetris.Point do
+  @moduledoc false
+
+  def origin do
+    {0, 0}
+  end
+
+  def left({x, y}) do
+    {x - 1, y}
+  end
+
+  def right({x, y}) do
+    {x + 1, y}
+  end
+
+  def down({x, y}) do
+    {x, y + 1}
+  end
+end
